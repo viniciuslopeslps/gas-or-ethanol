0cde115e283e7932da755a2dacb12e7064446192
Vinicius <viniciuslopeslps@gmail.com>
2016-02-17 21:33:26 -0200

    enviando dados

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,4 +2,9 @@ angular.module('myApp.controllers', ['ionic'])
 
 .controller('questionCtrl', function($state,$scope) {
 	$scope.nome = "vinicius";
+	$scope.calculate = function(data){
+		var gas = $scope.data.gas;
+		var ethanol = $scope.data.ethanol;
+		alert('gas : ' + gas + ' , ethanol : '+ ethanol);
+	};
 });
